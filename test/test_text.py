8ab514c2779c0f7b3c0d36fc3e3e2733aa231543
Knut Hühne <knut@k-nut.eu>
2017-06-19 12:07:25 +0200

    add basic tests setup

---
+++ test/test_text.py
@@ -0,0 +1,16 @@
+import unittest
+
+from normdatei.text import fingerprint
+
+
+class TestFingerprint(unittest.TestCase):
+    def test_none(self):
+        result = fingerprint(None)
+        assert result is None
+
+    def test_remove_title(self):
+        names = ["Dr Jakob Maria Mierscheid",
+                 "Dr. h.c. Jakob Maria Mierscheid",
+                 "Jakob Maria Mierscheid"]
+        normalized = {fingerprint(name) for name in names}
+        self.assertEqual(normalized, {"jakob-maria-mierscheid"})
